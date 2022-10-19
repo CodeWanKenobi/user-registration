524b7482c73060c9067f04a10feeb2ab0d247496
Kevin Nowak <nowakk96@icloud.com>
2022-10-19 14:54:08 +0200

    Initial commit

---
+++ src/test/java/com/github/codewankenobi/userregistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.codewankenobi.userregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
